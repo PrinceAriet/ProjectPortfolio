d3c71f37135a32cd115aaef3a89a90ac7c576822
PrinceAriet <95333329+PrinceAriet@users.noreply.github.com>
2022-01-09 22:56:49 +0600

    Update script-for-about.js

--- script-for-about.js
+++ script-for-about.js
@@ -23,23 +23,23 @@ function DarkMode() {
 let cursed = document.getElementById('cursed-img');
 cursed.onclick = function() {
     var bg = document.body;
-    bg.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('images/62ac9cbbc576f070768c1d4665fe42ad.jpg')";
+    bg.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('images/862a735726b51bf496b4820d1985e707.jpg')";
     bg.style.backgroundRepeat = "repeat";
     bg.style.backgroundSize = "contain";
     var main_block_bg = document.querySelector('.main-content');
-    main_block_bg.style.backgroundImage = "url('images/62ac9cbbc576f070768c1d4665fe42ad.jpg')";   
+    main_block_bg.style.backgroundImage = "url('images/862a735726b51bf496b4820d1985e707.jpg')";   
     var logo_bg = document.getElementById('logo');
-    logo_bg.style.backgroundImage = "url('images/62ac9cbbc576f070768c1d4665fe42ad.jpg')";
+    logo_bg.style.backgroundImage = "url('images/862a735726b51bf496b4820d1985e707.jpg')";
     var navLinks_bg = document.querySelector('.nav-links');
-    navLinks_bg.style.backgroundImage = "url('images/62ac9cbbc576f070768c1d4665fe42ad.jpg')";
+    navLinks_bg.style.backgroundImage = "url('images/862a735726b51bf496b4820d1985e707.jpg')";
     var strange_images = document.querySelector('.strange-img.one');
-    strange_images.src = "images/62ac9cbbc576f070768c1d4665fe42ad.jpg";
+    strange_images.src = "images/862a735726b51bf496b4820d1985e707.jpg";
     var strange_images2 = document.querySelector('.strange-img.two')
-    strange_images2.src = "images/62ac9cbbc576f070768c1d4665fe42ad.jpg";
+    strange_images2.src = "images/862a735726b51bf496b4820d1985e707.jpg";
     var strange_images3 = document.querySelector('.strange-img.three')
-    strange_images3.src = "images/62ac9cbbc576f070768c1d4665fe42ad.jpg";
+    strange_images3.src = "images/862a735726b51bf496b4820d1985e707.jpg";
     var pfp = document.querySelector('.pfp');
-    pfp.src = "images/62ac9cbbc576f070768c1d4665fe42ad.jpg";
+    pfp.src = "images/862a735726b51bf496b4820d1985e707.jpg";
     alert("You shouldn't have clicked...");
     console.log('I am working!');
 };
